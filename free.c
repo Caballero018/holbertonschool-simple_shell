b90f7b6b59ddcc10709ae0927c9d6df76a975105
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-08-18 19:39:47 -0700

    Changes in the project

--- free.c
+++ free.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
+ * freess - function that free the path and the list_token.
  *
- *
+ * @path: path.
+ * @list_token: list token.
  */
 void freess(char *path, char **list_token)
 {
@@ -10,6 +12,14 @@ void freess(char *path, char **list_token)
 	free_tokens(list_token);
 }
 
+/**
+ * freesss - function that free the path, the list_token and the command.
+ *
+ * @path: path.
+ * @list_token: list token.
+ * @command: command.
+ *
+ */
 void freesss(char *path, char **list_token, char *command)
 {
 	free(path);
